1f854a9f1e973f9b7a7fdd35734ba6d50f4ac20c
BrMatheusLopes <matheus1260@hotmail.com>
2022-12-20 09:30:15 -0300

    Adicionado HealthController

---
+++ HotmartAPI/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotmartAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
